809ede213db53a202e8aa19a5a157916c83c48a0
Kaique Lira <Kaiqueliira@gmail.com>
2021-03-03 12:45:47 -0300

    ajustado nome

---
+++ Models/Contato.cs
@@ -0,0 +1,12 @@
+namespace KlSolutions.Models
+{
+    public class Contato
+    {
+        
+        public string nome {get;set;}
+        public string email {get;set;}
+        public string assunto {get;set;}
+        public string mensagem {get;set;}
+
+    }
+}
